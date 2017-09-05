bce1866bfd57873d7b633e76fe89adc2cecfd456
Artur <qerentin@gmail.com>
2017-09-05 15:21:09 +0000

    Gagn heimaverkefni 1

---
+++ resources/views/threads/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Create a New Thread</div>
+
+                <div class="panel-body">
+                    <div class="input-group">
+                      <span class="input-group-addon" ></span>
+                      <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
