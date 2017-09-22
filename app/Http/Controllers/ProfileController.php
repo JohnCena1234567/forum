83a3bbbbe642d75c1d43b7da20a3600e43a4fdd5
Artur <qerentin@gmail.com>
2017-09-22 10:15:22 +0000

    Vefforritun5 Heimaverkefni2

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class ProfileController extends Controller
 {
     public function profile() {
-    	return view('profiles.profile');
+    	$threads = [
+    		['eigandi' => 'Artur Hirsz', 'title' => 'Laravel controller', 'body' => 'Hvernig byr madur til controller i Laravel?'],
+    		['eigandi' => 'Artur Hirsz', 'title' => 'Laravel 5.5', 'body' => 'Hver er munurinn a Laravel 5.4 og 5.5?']
+    	];
+
+    	return view('profiles.profile', compact('threads'));
     }
 }
--- resources/views/profiles/profile.blade.php
+++ resources/views/profiles/profile.blade.php
@@ -7,5 +7,25 @@
             <h1>Artur <small>Medlimur i 3 vikur</small></h1>
         </div>
     </div>
+    <br>
+    
+    <div class="col-md-6 col-md-offset-2">
+        @foreach($threads as $thread)
+        <ul class="list-group">
+            <li class="list-group-item">{{ $thread['eigandi'] }} published <a href="#">{{ $thread['title'] }}</a></li>
+            <li class="list-group-item">{{ $thread['body'] }}</li>
+        </ul>
+        @endforeach
+    </div>
+
+    <div class="panel panel-default col-md-3 ">
+        <div class="panel-body">
+            <h4>Nafn</h4>
+            <h5>Artur</h5>
+            <h4>Netfang</h4>
+            <h5>fs453486@fss.is</h5>
+            <h5>Fjoldi trada: 14</h5>
+        </div>
+    </div>
 </div>
 @endsection
