643532e3a670c89c3c2f7c12ccd3e6d63bd2f5b7
Artur <qerentin@gmail.com>
2017-09-26 15:10:54 +0000

    Seeder

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,6 @@ Route::get('/', function() {
 
 Route::get('/threads', 'ThreadsController@index');
 Route::post('/threads', 'ThreadsController@store');
-Route::get('/threads/create', 'ThreadsController@create')->middleware('auth');
+Route::get('/threads/create', 'ThreadsController@create');//->middleware('auth');
 Route::get('/threads/{id}', 'ThreadsController@show');
 Route::get('/profiles/{id}', 'ProfileController@profile');
